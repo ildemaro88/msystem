3de5ca1a46152f6de0bc8c97b74b7c4f174c9de1
ildemaro88 <ildemaro88@gmail.com>
2017-08-31 11:07:32 -0500

    DELETE: se eliminar archivo imoprt.js sin uso

--- public/js/paciente/import.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-		alert("hla");
-
-	console.log("{{$pacientesImportados}}"+"asd");
-	if("{{$pacientesImportados}}"){
-
-		$(".btn-success").attr('href',"{{route('listPacientes')}}");
-	}
-});
